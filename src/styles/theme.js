5b5e2e782d191062a28cf5dfbf464d6bd7642bdb
Humberto Galdino <humberto.galdino@live.com>
2024-05-31 09:14:05 -0300

    Create theme.js and global.js from styles

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF" /*TEXT */,
+    WHITE_100: "#E1E1E6" /*TITLES */,
+
+    GRAY_100: "#C4C4CC" /*LABELS */,
+    GRAY_200: "#FFFAF1" /*FOOTER */,
+    GRAY_300: "#7C7C8A" /*PLACEHOLDERS */,
+
+    BACKGROUND_DARK_900: "#0D1D25" /*LOGIN BACKGROUND */,
+  },
+};
