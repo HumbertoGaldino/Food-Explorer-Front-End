9af0a608af40f783ab531577718da4693b5cf243
Humberto Galdino <humberto.galdino@live.com>
2024-06-07 20:39:41 -0300

    Update FavoriteDishes

--- src/components/FavoriteDishes/index.jsx
+++ src/components/FavoriteDishes/index.jsx
@@ -4,12 +4,12 @@ import { ContainerFavoriteDishes } from "./style";
 export function FavoriteDishes({ data, onRemove }) {
   return (
     <ContainerFavoriteDishes>
-      <a href={`${api.defaults.baseURL}/dishes/${data.dish_id}`}>
+      <a href={`/dishes/${data.dish_id}`}>
         <img src={`${api.defaults.baseURL}/files/${data.image}`} alt="Dish image." />
       </a>
       
       <div>
-        <a href={`${api.defaults.baseURL}/dishes/${data.dish_id}`}>
+        <a href={`/dishes/${data.dish_id}`}>
           <h2>{data.name}</h2>
         </a>
         <button onClick={() => onRemove(data.id)}>Remover</button>
